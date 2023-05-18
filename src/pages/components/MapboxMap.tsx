0f6fc49d93bc9247084d23619d885d6eff11ece5
Jack Robetson <jackrobertsonps3@gmail.com>
2023-05-18 13:57:52 +0100

    Biuld Fix

--- src/pages/components/MapboxMap.tsx
+++ src/pages/components/MapboxMap.tsx
@@ -201,22 +201,22 @@ const MapboxMap = () => {
     if (elem && !document.fullscreenElement) {
       if (elem.requestFullscreen) {
         elem.requestFullscreen();
-      } else if (elem.mozRequestFullScreen) {
-        elem.mozRequestFullScreen();
-      } else if (elem.webkitRequestFullscreen) {
-        elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) {
-        elem.msRequestFullscreen();
+      } else if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.requestFullscreen) {
+        elem.requestFullscreen();
       }
     } else {
       if (document.exitFullscreen) {
         document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
+      } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.exitFullscreen) {
+        document.exitFullscreen();
       }
     }
   };
